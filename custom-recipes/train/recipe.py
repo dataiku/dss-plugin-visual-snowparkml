35152e39e0fa119ee34f25e964b50620f7ba47cc
pat <pat>
2023-11-02 02:32:52 +0000

    Edited files

--- custom-recipes/train/recipe.py
+++ custom-recipes/train/recipe.py
@@ -49,7 +49,7 @@ from snowflake.ml.modeling.tree import DecisionTreeClassifier, DecisionTreeRegre
 from snowflake.ml.modeling.linear_model import LogisticRegression, Lasso, PoissonRegressor, GammaRegressor, TweedieRegressor
 from snowflake.ml.modeling.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler, OrdinalEncoder
 from snowflake.ml.modeling.impute import SimpleImputer
-from snowflake.ml.modeling.metrics import accuracy_score, recall_score, f1_score, roc_auc_score, precision_score, r2_score, mean_absolute_error, mean_squared_error, d2_absolute_error_score, d2_pinball_score
+from snowflake.ml.modeling.metrics import accuracy_score, recall_score, f1_score, roc_auc_score, precision_score, r2_score, mean_absolute_error, mean_squared_error
 import snowflake.snowpark.functions as F
 from snowflake.ml.registry import model_registry
 
