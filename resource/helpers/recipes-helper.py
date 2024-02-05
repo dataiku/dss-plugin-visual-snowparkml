19aea172a50cac85dc374d6c1ceb7c9185eb30b6
pat <pat>
2024-02-05 22:08:08 +0000

    Edited files

--- resource/helpers/recipes-helper.py
+++ resource/helpers/recipes-helper.py
@@ -13,7 +13,7 @@ def do(payload, config, plugin_config, inputs):
         input_dataset_columns = [c for c in input_dataset.read_schema()]
         input_dataset_columns_new = []
         i = 1
-        for i, input_dataset_column in enumerate(input_dataset_columns, 1)
+        for i, input_dataset_column in enumerate(input_dataset_columns, 1):
             input_dataset_column["id"] = i
             input_dataset_columns_new.append(input_dataset_column)
             
