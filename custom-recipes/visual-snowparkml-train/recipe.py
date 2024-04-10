2617e41d02daec15ff98a096792c5cb9ac375d64
pat <pat>
2024-04-10 02:57:06 +0000

    Edited files

--- custom-recipes/visual-snowparkml-train/recipe.py
+++ custom-recipes/visual-snowparkml-train/recipe.py
@@ -564,7 +564,7 @@ for model in trained_models:
         print(col_label_values)
         test_f1 = f1_score(df = test_predictions_df, y_true_col_names = col_label_sf, y_pred_col_names = '"PREDICTION"', average = "macro")
         mlflow.log_metric("test_f1_score", test_f1)
-        test_roc_auc = roc_auc_score(df = test_prediction_probas_df, y_true_col_names = col_label_sf, y_score_col_names = target_col_value_cols, labels = col_label_values, average = "macro", multi_class = "ovo")
+        test_roc_auc = roc_auc_score(df = test_prediction_probas_df, y_true_col_names = col_label_sf, y_score_col_names = target_col_value_cols, average = "macro", multi_class = "ovo")
         mlflow.log_metric("test_roc_auc", test_roc_auc)
         test_accuracy = accuracy_score(df = test_predictions_df, y_true_col_names = col_label_sf, y_pred_col_names = '"PREDICTION"')
         mlflow.log_metric("test_accuracy", test_accuracy)
