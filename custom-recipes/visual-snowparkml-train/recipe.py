67b7aefcec02647a45cb18bb0506a85637c27940
pat <pat>
2024-04-10 19:41:19 +0000

    Edited files

--- custom-recipes/visual-snowparkml-train/recipe.py
+++ custom-recipes/visual-snowparkml-train/recipe.py
@@ -530,6 +530,11 @@ for model in trained_models:
         target_col_value_cols = [col for col in test_prediction_probas_df.columns if "PREDICT_PROBA" in col]
 
         test_f1 = f1_score(df = test_predictions_df, y_true_col_names = col_label_sf, y_pred_col_names = '"PREDICTION"', pos_label = col_label_values[0])
+        print("HIHIPAT")
+        test_predictions_df.show(5)
+        print(col_label_sf)
+        print(col_label_values[0])
+        
         mlflow.log_metric("test_f1_score", test_f1)
         test_roc_auc = roc_auc_score(df = test_prediction_probas_df, y_true_col_names = col_label_sf, y_score_col_names = test_prediction_probas_df.columns[-1])
         mlflow.log_metric("test_roc_auc", test_roc_auc)
